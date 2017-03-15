fa7c59dacdf05164a63fbce5230dcbbafd794484
Tyler Dickon <tdickon@kent.edu>
2017-03-15 16:03:25 -0400

    Update test_concat.cpp
    Added more testing, particularly dealing with larger strings, to make sure everything was sound.

--- String/test_concat.cpp
+++ String/test_concat.cpp
@@ -63,6 +63,32 @@ int main()
     assert(s1 == "abcdhfbcytwiayda");
     
   }
-  
+
+
+  {
+    String s1("Hello Mr.");
+    char ch('C');
+    String result = s1 + ch;
+    std::cerr << "result: " << result; std::cout << std::endl;
+    assert(result == "Hello Mr.C");
+  }
+
+  {
+    String s1("Hello Mr.");
+    char *ch  = " Alex Mahan, the world's greatest programmer.";
+    String result = s1 + ch;
+    std::cerr << "result: " << result; std::cout << std::endl;
+    assert(result == "Hello Mr. Alex Mahan, the world's greatest programmer.");
+  }
+
+  { 
+    String s1("Zabuza Momochi: ");
+    String s2("*to Naruto* Your words cut deeper than any blade.");
+    String s3(" Well? Cat got your tongue? Are you surprised to discover that I am human? Even shinobi are human. No matter how hard we try, we always fail to escape that simple fact. Well at least, I have failed.");
+    String result = s1 + s2 + s3;
+    std::cerr << "result: " << result; std::cout << std::endl;
+
+    assert(result =="Zabuza Momochi: *to Naruto* Your words cut deeper than any blade. Well? Cat got your tongue? Are you surprised to discover that I am human? Even shinobi are human. No matter how hard we try, we always fail to escape that simple fact. Well at least, I have failed."); 
+  }
+
   std::cout << "COMPLETED TESTING CONCATINATION" << std::endl;
-}
